0d61a3e9682752a9de7f6ef724e8ae3f098ff6ec
Tr0cker <trash1760@gmail.com>
2022-11-17 23:26:00 +0000

    updated

--- 1st draft doraemon/Assets/Script/PauseMenu.cs
+++ 1st draft doraemon/Assets/Script/PauseMenu.cs
@@ -7,6 +7,9 @@ public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _uiButton_left;
+    [SerializeField] private GameObject _uiButton_right;
+    [SerializeField] private GameObject _uiButton_jump;
     [SerializeField] private GameObject _pausebutton;
 
     public void StartGame()
@@ -30,6 +33,11 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         _pauseMenu.SetActive(true);
         _pausebutton.SetActive(false);
+
+        //hide mobile controller button
+        _uiButton_left.SetActive(false);
+        _uiButton_right.SetActive(false);
+        _uiButton_jump.SetActive(false);
     }
 
     public void ResumeButton()
@@ -37,6 +45,11 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1.0f;
         _pauseMenu.SetActive(false);
         _pausebutton.SetActive(true);
+
+        //show mobile controller button
+        _uiButton_left.SetActive(true);
+        _uiButton_right.SetActive(true);
+        _uiButton_jump.SetActive(true);
     }
     //void Start()
     //{
