4fc698e007e4db7eaacf41f37946ed4d47401b7b
Tr0cker <trash1760@gmail.com>
2022-11-23 13:44:18 +0000

    camera fixed

--- 1st draft doraemon/Assets/Script/CameraController.cs
+++ 1st draft doraemon/Assets/Script/CameraController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    public float yOffset = 1f;
 
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, yOffset, transform.position.z);
     }
 }
