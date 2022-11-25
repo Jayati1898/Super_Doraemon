c1d46278953f150149341e3c5675fb9f9f47c306
Tr0cker <trash1760@gmail.com>
2022-11-25 13:15:54 +0000

    enemy flip script

--- 1st draft doraemon/Assets/Script/WaypointFollower.cs
+++ 1st draft doraemon/Assets/Script/WaypointFollower.cs
@@ -7,12 +7,23 @@ public class WaypointFollower : MonoBehaviour
     [SerializeField] private GameObject[] waypoints;
     private int currentWaypointIndex = 0;
 
+    private Rigidbody2D E_rb;
+    private SpriteRenderer E_Sprite;
+
     [SerializeField] private float speed = 2f;
+
+    void Start()
+    {
+        E_rb = GetComponent<Rigidbody2D>();
+        E_Sprite = GetComponent<SpriteRenderer>();
+
+    }
 
     private void Update()
     {
         if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
         {
+            E_Sprite.flipX = !E_Sprite.flipX;
             currentWaypointIndex++;
             if (currentWaypointIndex >= waypoints.Length)
             {
@@ -20,5 +31,6 @@ public class WaypointFollower : MonoBehaviour
             }
         }
         transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+        
     }
 }
