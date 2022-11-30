0d89e43756fb0d457e29438b7124757e77382dca
Tr0cker <trash1760@gmail.com>
2022-11-30 01:59:56 +0000

    update

---
+++ 1st draft doraemon/Assets/Script/Shoot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
