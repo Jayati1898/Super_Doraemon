32dac8ae338bf6f92656c9e24b1cf76e6abeac19
Tr0cker <trash1760@gmail.com>
2022-12-01 21:42:50 +0000

    update

--- 1st draft doraemon/Assets/Script/CameraController_level2.cs
+++ 1st draft doraemon/Assets/Script/CameraController_level2.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController_level2 : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    public float yOffset = 1f;
 
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, yOffset, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y - 1.5f, transform.position.z);
     }
 }
